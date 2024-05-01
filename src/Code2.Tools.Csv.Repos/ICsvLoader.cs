61225de47fa465d065a7eaee55fa0553d6cefc1b
2code <2mail2code@gmail.com>
2024-05-01 21:19:34 +0200

    add csvReaderError event to ICsvLoader interface

--- src/Code2.Tools.Csv.Repos/ICsvLoader.cs
+++ src/Code2.Tools.Csv.Repos/ICsvLoader.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Code2.Tools.Csv.Repos
 {
 	public interface ICsvLoader
 	{
+		event EventHandler<UnhandledExceptionEventArgs>? CsvReaderError;
+
 		Task LoadAsync(string[]? targetTypes = null);
 		Task LoadFileAsync(string fileNameOrFilter, bool clearRepository = true);
 		Task LoadFileAsync(CsvFileInfo fileInfo, bool clearRepository = true);
